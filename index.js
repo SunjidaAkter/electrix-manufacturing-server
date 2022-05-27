5c9f13886fda1695528f8b72b3e316a25a30dfaa
SunjidaAkter <sanjidabintesayid7@gmail.com>
2022-05-28 00:27:01 +0600

    almost done

--- index.js
+++ index.js
@@ -14,25 +14,17 @@ app.use(express.json());
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
-        return res.status(401).send({ massage: "Sorry Unauthorized" });
+        return res.status(401).send({ message: 'UnAuthorized access' });
     }
-
-    jwt.verify(
-        authHeader,
-        process.env.ACCESS_TOKEN_SECRET,
-        function (err, decoded) {
-            if (err) {
-                return res
-                    .status(403)
-                    .send({ massage: " Forbidden, does not have access " });
-            }
-
-            req.decoded = decoded;
-            next();
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
         }
-    );
+        req.decoded = decoded;
+        next();
+    });
 }
-
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.daybt.mongodb.net/?retryWrites=true&w=majority`;
@@ -62,6 +54,34 @@ async function run() {
         }
 
 
+        // app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+        //     const service = req.body;
+        //     const price = service.price;
+        //     const amount = price * 100;
+        //     const paymentIntent = await stripe.paymentIntents.create({
+        //         amount: amount,
+        //         currency: 'usd',
+        //         payment_method_types: ['card']
+        //     });
+        //     res.send({ clientSecret: paymentIntent.client_secret })
+        // });
+
+
+        app.post("/review", async (req, res) => {
+            const doc = req.body;
+            const result = await reviewCollection.insertOne(doc);
+            res.send(result);
+        });
+
+
+
+        app.post("/adminTool", verifyJWT, verifyAdmin, async (req, res) => {
+            const doc = req.body;
+            const result = await toolCollection.insertOne(doc);
+            res.send(result);
+            console.log(result);
+        });
+
 
         app.post("/order", async (req, res) => {
             const doc = req.body;
@@ -79,8 +99,43 @@ async function run() {
                 $set: user,
             };
             const result = await userCollection.updateOne(filter, updateDoc, options);
-            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '100d' })
             res.send({ result, token });
+        });
+
+        app.put('/currentUser/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    phone: user.phone,
+                    address: user.address,
+                    study: user.study,
+                    facebook: user.facebook,
+                    instagram: user.instagram,
+                    twitter: user.twitter
+                }
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            res.send({ result });
+        });
+
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+
+        app.get('/user', verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
         });
 
 
@@ -92,6 +147,15 @@ async function run() {
         })
 
 
+        app.get('/currentUser', verifyJWT, async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const result = await userCollection.findOne(query);
+            res.send(result);
+
+        })
+
+
         app.get('/tool', async (req, res) => {
             const query = {};
             const cursor = toolCollection.find(query);
@@ -99,11 +163,20 @@ async function run() {
             res.send(tools);
         })
 
-
-        app.get('/tool/:id', async (req, res) => {
+        app.get('/currentTool/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await toolCollection.findOne(query);
+            res.send(result);
+            console.log(result)
+
+
+        })
+
+        app.get('/myOrder', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const result = await orderCollection.find(query).toArray();
             res.send(result);
 
         })
@@ -122,6 +195,14 @@ async function run() {
             const cursor = newsCollection.find(query);
             const news = await cursor.toArray();
             res.send(news);
+        });
+
+
+        app.delete("/myOrder/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
         });
 
     }
